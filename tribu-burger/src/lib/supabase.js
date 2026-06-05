0159d92a836134d2befe8724072de6367fcb8b83
Matias Tasi <mati40115666@gmail.com>
2026-06-04 23:18:42 -0300

    fix: estructura del repositorio limpia

--- tribu-burger/src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://jhfyfdllfzxtciibsfri.supabase.co'
-const supabaseKey = 'sb_publishable_Q6pe_RH2AwtkbZGDz2uu2w_JrVcQ6qo'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
